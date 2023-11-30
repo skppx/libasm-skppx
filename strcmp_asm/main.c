917fb1611d8b049e1410b417b04bd580561744e0
phabets <phabets@student.42.fr>
2023-11-30 20:37:02 +0100

    strcmp

---
+++ strcmp_asm/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+extern size_t ft_strcmp(const char *s1, const char *s2);
+
+int main()
+{
+    const char *s1 = "\200";
+    const char *s2 = NULL;
+
+    int res = strcmp(s1, s2);
+    int res_asm = ft_strcmp(s1, s2);
+
+    printf("res :%i, res_asm:%i", res, res_asm);
+
+    return 0;
+}
